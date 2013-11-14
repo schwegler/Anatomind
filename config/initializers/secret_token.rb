21b775b6583ae208e3494e3d2be6fe9a09c7f12a
schwegler <schwegler@gmail.com>
2013-11-13 23:03:15 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anatomind::Application.config.secret_key_base = 'ffb5e6917bb045fa159387b73df7cd0d86b3037b8850130cd7cd735ca05535158f246e60b9a3c9b2caf0bb8510b5a5d75cb7e7d89d513e5255633d3df2433a5a'
